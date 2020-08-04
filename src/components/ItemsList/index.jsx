838ec63d0bf78a1abada4a0babaab4f212e62f3c
Ellie04162606 <1181140808@qq.com>
2020-08-04 21:53:57 +0800

    [ToDoList][Ellie & Jack] feature: call get api and get items successful

--- src/components/ItemsList/index.jsx
+++ src/components/ItemsList/index.jsx
@@ -1,22 +1,50 @@
 import React, { Component } from "react";
 import Item from "../Item";
 import { connect } from "react-redux";
+import Axios from "axios";
 class ItemList extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            TodoList:[]
+        }
+    }
   render() {
     console.log(this.props.items);
     return (
       <div>
-        {this.props.items.map((item, index) => (
+        {/* {this.props.items.map((item, index) => (
           <Item
             key={index}
             index={index}
-            content ={item.content}
+            content={item.content}
             status={item.status}
             deleteItem={this.props.deleteItem}
             handleMark={this.props.handleMark}
           />
-        ))}
+        ))} */}
+        {this.state.TodoList
+          ? this.state.TodoList.map((item, index) => (
+              <Item
+                key={index}
+                index={index}
+                content={item.content}
+                status={item.status}
+                deleteItem={this.props.deleteItem}
+                handleMark={this.props.handleMark}
+              />
+            ))
+          : null}
       </div>
+    );
+  }
+
+  componentDidMount(){
+    Axios.get("https://5e9ec500fb467500166c4658.mockapi.io/todos").then(
+      (res) => {
+            console.log(res.data);
+          this.setState({ TodoList: res.data });
+      }
     );
   }
 }
